8cc79b70b9318e0dbb48053a5866ab879947c15d
Alabhya Vaibhav <alabhya.vaibhav1997@gmail.com>
2017-07-13 13:51:40 +0530

    Lab 1
    Can handle decimal Division

--- 2-13-07-17.sh
+++ 2-13-07-17.sh
@@ -5,12 +5,12 @@ echo Enter Value  B
 read b
 c=`expr $a + $b`
 d=`expr $a - $b`
-e=`expr $a \*  $b`
-f=`expr $a / $b`
+e=`expr $a \* $b`
+result=$(echo "scale=3; $a / $b" | bc)
 g=`expr $a % $b`
 echo Add= $c
 echo Subtract= $d
 echo Multiply= $e
-echo Divde= $f
+echo "Divide = $result"    
 echo Mod= $g
 
